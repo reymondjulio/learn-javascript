6dbf36b22f9bde474e1d6e514b651d9a0632a575
Reymond Julio <reymondjulio58@gmail.com>
2023-07-07 12:28:58 +0700

    implement dom friends

---
+++ dom-friends-copy/index.js
@@ -0,0 +1,84 @@
+// Reymond Julio
+const parentElement1 = document.getElementById("friends-1");
+const buttonElement1 = document.getElementById("button-1");
+const deleteElement1 = document.getElementById("delete-1");
+const reymond = document.getElementById("reymond-image");
+
+// Confirm Function
+buttonElement1.addEventListener("click", function () {
+  alert("Request Accepted");
+});
+
+// Delete Function
+deleteElement1.addEventListener("click", function () {
+  reymond.remove();
+  parentElement1.remove();
+});
+
+// Haidar Hanif
+const parentElement2 = document.getElementById("friends-2");
+const buttonElement2 = document.getElementById("button-2");
+const deleteElement2 = document.getElementById("delete-2");
+const haidar = document.getElementById("haidar-image");
+
+// Confirm Function
+buttonElement2.addEventListener("click", function () {
+  alert("Request Accepted");
+});
+
+// Delete Function
+deleteElement2.addEventListener("click", function () {
+  haidar.remove();
+  parentElement2.remove();
+});
+
+// Caroline
+const parentElement3 = document.getElementById("friends-3");
+const buttonElement3 = document.getElementById("button-3");
+const deleteElement3 = document.getElementById("delete-3");
+const caroline = document.getElementById("caroline-image");
+
+// Confirm Function
+buttonElement3.addEventListener("click", function () {
+  alert("Request Accepted");
+});
+
+// Delete Function
+deleteElement3.addEventListener("click", function () {
+  caroline.remove();
+  parentElement3.remove();
+});
+
+// Vicky
+const parentElement4 = document.getElementById("friends-4");
+const buttonElement4 = document.getElementById("button-4");
+const deleteElement4 = document.getElementById("delete-4");
+const vicky = document.getElementById("vicky-image");
+
+// Confirm Function
+buttonElement4.addEventListener("click", function () {
+  alert("Request Accepted");
+});
+
+// Delete Function
+deleteElement4.addEventListener("click", function () {
+  vicky.remove();
+  parentElement4.remove();
+});
+
+// Christopher
+const parentElement5 = document.getElementById("friends-5");
+const buttonElement5 = document.getElementById("button-5");
+const deleteElement5 = document.getElementById("delete-5");
+const christopher = document.getElementById("christopher-image");
+
+// Confirm Function
+buttonElement5.addEventListener("click", function () {
+  alert("Request Accepted");
+});
+
+// Delete Function
+deleteElement5.addEventListener("click", function () {
+  christopher.remove();
+  parentElement5.remove();
+});
