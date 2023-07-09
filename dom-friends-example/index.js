2ca9deed025d9bd015317805a08429745c7a05d9
M Haidar Hanif <git@mhaidarhanif.com>
2023-07-09 14:30:26 +0700

    render friends with better syling

--- dom-friends-example/index.js
+++ dom-friends-example/index.js
@@ -6,7 +6,7 @@ let dataFriends = [
     mutualFriends: 5,
   },
   {
-    imageURL: "images/haidar.png",
+    imageURL: "images/haidar.jpg",
     name: "M Haidar Hanif",
     age: 30,
     mutualFriends: 6,
@@ -17,7 +17,34 @@ const friendsListElement = document.getElementById("friends-list");
 
 function renderFriends() {
   dataFriends.forEach((friend) => {
-    console.log(friend.name);
+    const friendItemElement = document.createElement("li"); // LI
+
+    friendItemElement.innerHTML = `
+    <div>
+      <img
+        src=${friend.imageURL}
+        alt=${friend.name}
+        class="rounded object-cover w-60 h-60"
+      />
+    </div>
+    <div class="flex flex-col bg-slate-200 p-6 mb-6 space-y-2">
+      <h3 class="text-center text-xl font-semibold">${friend.name}</h3>
+      <p class="text-center text-lg text-slate-800">${friend.age} years old</p>
+      <p class="text-center text-lg text-slate-500">${friend.mutualFriends} mutual friends</p>
+      <button
+        class="bg-blue-500 text-white p-2 rounded w-full self-center hover:bg-blue-400"
+      >
+        Confirm
+      </button>
+      <button
+        class="bg-slate-300 rounded text-black font-semibold p-2 w-full self-center hover:bg-slate-400"
+      >
+        Delete
+      </button>
+    </div>
+    `;
+
+    friendsListElement.appendChild(friendItemElement);
   });
 }
 
