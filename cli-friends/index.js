04e77c1f6f226c3cfc85d57c101c89ded43e8527
M Haidar Hanif <git@mhaidarhanif.com>
2023-07-09 15:53:09 +0700

    plan cli friends

---
+++ cli-friends/index.js
@@ -0,0 +1,24 @@
+const dataFriends = [{ id: 1, name: "Reymond" }];
+
+function renderFriends() {
+  dataFriends.forEach((friend) => {
+    console.log(friend);
+  });
+}
+
+function deleteFriend() {}
+
+function confirmFriend() {}
+
+function addFriend() {
+  // HINT
+  // dataFriends = [...dataFriends, { id: 10, name: "Siapa" }];
+}
+
+function searchFriend() {
+  // HINT
+  // dataFriends.find = 1 object
+  // dataFriends.filter = >1 objects
+}
+
+renderFriends();
